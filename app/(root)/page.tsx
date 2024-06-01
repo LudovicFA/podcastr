54330f0a7384bd0d8afca0590a5bd34c26c15920
Ludovic FAVRE ARTIGUES <ludovic.favreartigues@gmail.com>
2024-06-01 15:15:38 +0200

    homepage layout

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,13 +1,25 @@
+import PodcastCard from "@/components/PodcastCard"
 import { Button } from "@/components/ui/button"
+import { podcastData } from "@/constants"
 
 const Home = () => {
   return (
     <div className="mt-9 flex flex-col gap-9">
       <section className="flex flex-col gap-5">
         <h1 className="text-20 font-bold text-white-1 ">Trending Podcast</h1>
-        <Button className="text-white-1 bg-orange-1">
-          Click me
-        </Button>
+        <div className="podcast_grid">
+          {podcastData.map(({description, id, imgURL, title}) => (
+
+            <PodcastCard 
+              key={id}
+              imgUrl={imgURL}
+              title={title}
+              description={description}
+              podcastId={id}
+            />
+          ))}
+
+        </div>
       </section>
     </div>
   )
