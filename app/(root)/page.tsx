a1e0cb519a81c5859e567291f66b4fea3f8bdb25
Ludovic FAVRE ARTIGUES <ludovic.favreartigues@gmail.com>
2024-06-01 16:40:42 +0200

    convex & clerk setup + webhooks

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,12 +1,20 @@
+"use client"
 import PodcastCard from "@/components/PodcastCard"
-import { Button } from "@/components/ui/button"
 import { podcastData } from "@/constants"
+import { api } from "@/convex/_generated/api";
+import { useQuery } from "convex/react";
 
 const Home = () => {
+
+  // const tasks = useQuery(api.tasks.get);
+
   return (
     <div className="mt-9 flex flex-col gap-9">
       <section className="flex flex-col gap-5">
         <h1 className="text-20 font-bold text-white-1 ">Trending Podcast</h1>
+        {/* <div className="text-white-1">
+          {tasks?.map(({ _id, text }) => <div key={_id}>{text}</div>)}
+        </div> */}
         <div className="podcast_grid">
           {podcastData.map(({description, id, imgURL, title}) => (
 
