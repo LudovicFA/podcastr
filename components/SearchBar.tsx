a74d316e092c6f3eec5cd34ebfea15a3fbc873f3
Ludovic FAVRE ARTIGUES <ludovic.favreartigues@gmail.com>
2024-06-04 14:16:04 +0200

    Discover page

---
+++ components/SearchBar.tsx
@@ -0,0 +1,45 @@
+
+"use client"
+
+import React, { useEffect, useState } from 'react'
+import { Input } from './ui/input'
+import Image from 'next/image'
+import { usePathname, useRouter } from 'next/navigation'
+import { useDebounce } from '@/lib/useDebounce'
+
+const SearchBar = () => {
+    const [search, setSearch] = useState('')
+    const router = useRouter();
+    const pahtname = usePathname();
+    const debounceValue  = useDebounce(search, 500)
+
+    useEffect(() => {
+        if(debounceValue){
+            router.push(`/discover?search=${search}`)
+        }
+        else if(pahtname === '/discover'){
+            router.push('/discover')
+        }
+    }, [search, router, pahtname])
+
+  return (
+    <div className='relative mt-8 block'>
+        <Input
+            className='input-class py-6 pl-12 focus-visible:ring-offset-orange-1'
+            placeholder='Search for podcast'
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+            onLoad={()=> setSearch('')}
+        />
+        <Image
+            src={"/icons/search.svg"}
+            alt='search'
+            height={20}
+            width={20}
+            className='absolute left-4 top-3.5'
+        />
+    </div>
+  )
+}
+
+export default SearchBar
